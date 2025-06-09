a1bcf8c18ac082e29e5ff4f3c037a49a3c92182f
Roman Kitaev <802.11g@bk.ru>
2025-06-09 14:12:42 +0300

    rolling hash

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,13 +1,22 @@
-use std::{fs, hint::black_box};
-use criterion::{criterion_group, criterion_main, Criterion};
 use bitcut::make_diff;
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::{fs, hint::black_box};
 
-
-fn criterion_benchmark(c: &mut Criterion) {
-    let old = fs::read("/Users/kitaev/projects/tochka/fds-rs/300990012_new.cbor").unwrap();
-    let new = fs::read("/Users/kitaev/projects/tochka/fds-rs/300990012_newnew.cbor").unwrap();
-    c.bench_function("make_diff", |b| b.iter(|| make_diff(black_box(&old), black_box(&new))));
+fn make_diff_huge(c: &mut Criterion) {
+    let old = fs::read("benches/fixtures/huge_old").unwrap();
+    let new = fs::read("benches/fixtures/huge_new").unwrap();
+    c.bench_function("make_diff_huge", |b| {
+        b.iter(|| make_diff(black_box(&old), black_box(&new)))
+    });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn make_diff_p90(c: &mut Criterion) {
+    let old = fs::read("benches/fixtures/p90_old").unwrap();
+    let new = fs::read("benches/fixtures/p90_new").unwrap();
+    c.bench_function("make_diff_p90", |b| {
+        b.iter(|| make_diff(black_box(&old), black_box(&new)))
+    });
+}
+
+criterion_group!(benches, make_diff_huge, make_diff_p90);
 criterion_main!(benches);
